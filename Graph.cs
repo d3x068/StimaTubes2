5e987529312c441c7ba50a4b6b32c4961b48f0d5
Wildan Zaim Syaddad <52845131+zaimsyaddad99@users.noreply.github.com>
2020-03-09 19:30:39 +0700

    Node, Setter, Getter, ctor

--- Graph.cs
+++ Graph.cs
@@ -1,14 +1,54 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Graph
 {
-    class Graph
+    class Program
     {
-        
-    }
+        // public class Graph
+        // {
+        //     public 
+        // }
 
-    class Node
-    {
+        public class Node
+        {
+            public string Kota;
+            public int Populasi;
+            List<Node> NodesList = new List<Node>();
 
+            public Node(string Kota,int Populasi)
+            {
+                this.Kota = Kota;
+                this.Populasi = Populasi;
+            }
+
+            public string GetKota(){
+                return this.Kota;
+            }
+
+            public void SetKota(string Kota){
+                this.Kota = Kota;
+            }
+
+            public int GetPopulasi(){
+                return this.Populasi;
+            }
+
+            public void SetPopulasi(int Populasi){
+                this.Populasi = Populasi;
+            }
+
+            // public List<Node> Nodes
+            // {
+
+            // }
+        }
+
+        static void Main(string[] args)
+        {
+            Node N = new Node("A",100000);
+        }
     }
 }
